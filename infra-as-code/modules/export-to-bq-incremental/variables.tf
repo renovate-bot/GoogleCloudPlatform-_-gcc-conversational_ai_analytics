221ff338c924317f26e982cdca6d573207705a74
julianogando <julianogando@google.com>
2024-09-02 12:32:04 +0000

    add variable description

--- infra-as-code/modules/export-to-bq-incremental/variables.tf
+++ infra-as-code/modules/export-to-bq-incremental/variables.tf
@@ -14,46 +14,57 @@
 
 variable "project_id" {
   type = string
+  description = "Project ID in which the resources will be provisioned"
 }
 
 variable "region" {
   type = string
+  description = "Region in which the resources will be provisioned"
 }
 
 variable "ccai_insights_project_id" {
   type = string
+  description = "Project ID of the CCAI Insights instance"
 }
 
 variable "ccai_insights_location_id" {
   type = string
+  description = "Location ID of the CCAI Insights instance"
 }
 
 variable "bigquery_project_id" {
   type = string
+  description = "Project ID to which we will be sending the CCAI Insights data to BigQuery"
 }
 
 variable "bigquery_staging_dataset" {
   type = string
+  description = "BigQuery dataset in which we will be writing the Staging data"
 }
 
 variable "bigquery_final_dataset" {
   type = string
+  description = "BigQuery dataset in which we will be writing the data"
 }
 
 variable "bigquery_staging_table" {
   type = string
+  description = "BigQuery table in which we will be writing the Staging data"
 }
 
 variable "bigquery_final_table" {
   type = string
+  description = "BigQuery table in which we will be writing the data"
 }
 
 variable "export_to_bq_cron" {
   type = string
+  description = "CRON expression that defines how often the CCAI Insights data will be exported"
 }
 
 variable "service_account_id" {
   type = string
+  description = ""
 }
 
 variable "cf_bucket_name" {
